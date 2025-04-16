9341bd9ef9f41253fd97d4c0e48a34bf8ea2ed6d
George-25 <georgemallory26@gmail.com>
2025-04-15 20:20:33 -0400

    added tailwindcss in react

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>Hello components</h1>
+      <h1 className="text-green-500">Hello components</h1>
     </div>
   );
 };
