0ebb78e469da616e2df571ac12c07102dc7bce0b
George-25 <georgemallory26@gmail.com>
2025-04-17 21:11:31 -0400

    added name input in user profile

--- src/components/UserProfileForm.jsx
+++ src/components/UserProfileForm.jsx
@@ -2,9 +2,34 @@ import React from "react";
 
 const UserProfileForm = () => {
   return (
-    <div>
-      <h1>I am user profile form</h1>
-    </div>
+    <>
+      <div className="container">
+        <h1>User Profile</h1>
+        <form className="border">
+          {/* firstname and lastname */}
+          <div className="mb-4">
+            <label className="form-label">Name</label>
+            <input
+              type="text"
+              name="firstname"
+              placeholder="First Name"
+              className="border"
+            />
+            <input
+              type="text"
+              name="lastname"
+              placeholder="Last Name"
+              className="border"
+            />
+          </div>
+        {/* email portion */}
+        
+<div>
+
+</div>
+        </form>
+      </div>
+    </>
   );
 };
 
