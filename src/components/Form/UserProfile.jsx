81d08053ff51b63db8702c7e6b1c466e6b6dae7a
George-25 <georgemallory26@gmail.com>
2025-04-21 22:31:35 -0400

    added some codes in new form

--- src/components/Form/UserProfile.jsx
+++ src/components/Form/UserProfile.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
 const UserProfile = () => {
+  const [userForm, setUserForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    gender: "default",
+  });
   return (
     <div>
-      <h1>Hello I am Reusable form.</h1>
+      <form>
+        <div>
+          <label htmlFor="firstName">FirstName:</label>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="Enter your first name"
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">LastName:</label>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Enter your last name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" placeholder="Enter your email" />
+        </div>
+        <div>
+          <label htmlFor="gender">Gender:</label>
+          <input type="text" name="gender" />
+        </div>
+      </form>
     </div>
   );
 };
