0e568495f2be1b792ca6a16500f8bd39e94465f3
George-25 <georgemallory26@gmail.com>
2025-05-16 18:54:04 -0400

    login detail done

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const LoginForm = () => {
   return (
     <div className="container">
-      <form className="border"></form>
+      <form className="border">
+        <label htmlFor="username">
+          <b>Username</b>
+        </label>
+        <input type="text" placeholder="Enter Username" name="username" />
+        <label htmlFor="password">
+          <b>Password</b>
+        </label>
+        <input type="password" placeholder="Enter Password" name="password" />
+        <label htmlFor="checkbox">
+          <input type="checkbox" checked="checked" name="remember" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
