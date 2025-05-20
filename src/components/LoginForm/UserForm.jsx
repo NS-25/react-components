157f6050ddac558a4f95126a932ae8ecd32a6f61
George-25 <georgemallory26@gmail.com>
2025-05-19 22:29:36 -0400

    added some code

--- src/components/LoginForm/UserForm.jsx
+++ src/components/LoginForm/UserForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { use } from "react";
+
 import { useState } from "react";
 import "./UserForm.css";
 
@@ -9,6 +9,44 @@ const UserForm = () => {
   const [checked, setChecked] = useState(false);
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
+
+  const validateUsername = () => {
+    console.log("validateUsername called");
+    // username validation //
+    if (!username) {
+      return "Username is required.";
+    }
+    if (username.length < 3) {
+      return "Username must be at least 3 characters.";
+    }
+    if (username.length > 15) {
+      return "Username must be under 15 characters.";
+    }
+    if (!/^[a-zA-Z0-9_]+$/.test(username)) {
+      return "Only letters, numbers, and underscores allowed.";
+    }
+
+    return "";
+  };
+
+  const validatePassword = () => {
+    // password validation //
+    if (!password) {
+      return "Password is required.";
+    }
+    if (password.length < 6) {
+      return "Password must be at least 6 characters.";
+    }
+    return "";
+  };
+
+  const validateTerm = () => {
+    // checked validation //
+    if (!checked) {
+      return "You must accept the terms.";
+    }
+    return "";
+  };
 
   const validation = () => {
     // username validation //
@@ -36,38 +74,71 @@ const UserForm = () => {
     if (!checked) {
       return "You must accept the terms.";
     }
+    return "";
+  };
+
+  //Handle form submit //
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // const validationError = validation();
+    // if (validationError) {
+    //   setError(validationError);
+    //   setSuccess("");
+    // } else {
+    //   setError("");
+    //   setSuccess("Form submitted successfully!");
+    // }
+  };
+
+  
+
+  const handleBlurPassword = () => {
+    return;
   };
 
   return (
     <div className="login-form-wrapper">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-          placeholder="Enter Username"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          placeholder="Enter Password"
-        />
-        <label htmlFor="checkbox">
+        <div className="mb-2">
           <input
-            type="checkbox"
-            name="checked"
-            checked={checked}
-            onChange={(e) => setChecked(e.target.checked)}
+            type="text"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            onBlur={() => {
+              const error = validateUsername();
+              setError(error);
+            }}
           />
-          Accept Terms & Conditions
-        </label>
+        </div>
+        <div className="mb-2">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            onBlur={() => {
+              const error = validatePassword();
+              setError(error);
+            }}
+            placeholder="Enter Password"
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="checkbox">
+            <input
+              type="checkbox"
+              name="checked"
+              checked={checked}
+              onChange={(e) => setChecked(e.target.checked)}
+            />
+            Accept Terms & Conditions
+          </label>
+        </div>
         <div>
           {error && <p className="field-error">{error}</p>}
           {success && <p className="field-success">{success}</p>}
